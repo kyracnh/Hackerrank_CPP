78584c4482f19bbca03a949b830ea5311cd919bb
root <root@Onmazir.localdomain>
2024-09-12 22:36:22 +0100

    Input and Output

---
+++ Input_and_Output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    cin >>  a >> b >> c;
+    int result = a + b + c;
+    cout << result;
+    return 0;
+}
